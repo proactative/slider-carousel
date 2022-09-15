79b3a8eb45e5b1cadd7286bf3a3078edaf526d25
Cyril Lazar <klazar@mail.ru>
2022-09-15 19:39:13 +1000

    дописал транслейтХ

--- sliderjs.js
+++ sliderjs.js
@@ -1,16 +1,19 @@
 const buttonLeft = document.querySelector('.slider__button-left');
 const buttonRight = document.querySelector('.slider__button-right');
 const sliderRow = document.querySelector('.slider__row');
+const sliderItem = document.querySelector('.slider__element')
 let offset = 0; //смещеие от левого края
+
+console.log(sliderItem.offsetWidth);
 
 function movingForward(){
     offset = offset + 854;
-    sliderRow.style.translate = offset + 'px';
+    sliderRow.style.transform = `translateX(${offset}px)`;
 }
 
 function movingBackward(){
     offset = offset - 854;
-    sliderRow.style.translate = offset + 'px';
+    sliderRow.style.transform = `translateX(${offset}px)`;
 }
 
 buttonLeft.addEventListener("click", movingForward);
