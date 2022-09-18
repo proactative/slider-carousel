3eb50f9a30d7bfc1f0f01d613e21d6ebfe2a3c6f
Cyril Lazar <klazar@mail.ru>
2022-09-18 14:40:47 +1000

    добавил слайдинг колесиком

--- sliderjs.js
+++ sliderjs.js
@@ -36,9 +36,10 @@ makeArray(array);
 const rowItems = document.querySelectorAll('.slider__element');
 const rowItemWidth = rowItems[0].offsetWidth;
 
+const offset = 174; 
 const gap = 30;
-const start = -rowItemWidth + 174 - gap;//начальное положение: ширина карточки + смешение блока - 
-const finish = - (rowItemWidth+gap)*array.length + 174;//конечное положение
+const start = -rowItemWidth + offset - gap;//начальное положение: ширина карточки + смешение блока - 
+const finish = - (rowItemWidth+gap)*array.length + offset;//конечное положение
 console.log(start, '##', finish);
 let index = 0; 
 let allowShift = true;
@@ -51,6 +52,17 @@ buttonRight.addEventListener("click", ()=>{
 });//право
 
 sliderRow.addEventListener('transitionend', checkIndex);
+
+sliderRow.addEventListener('wheel', movingByWheel);
+
+
+function movingByWheel(e) {
+    e.preventDefault();
+
+    if(e.deltaY<0) {
+        movingClide(-1);
+    } else if(e.deltaY>0) movingClide(1);
+}
 
 function movingClide(dir){
     sliderRow.classList.add('moving');
@@ -74,8 +86,9 @@ function movingClide(dir){
 
 
 
-function checkIndex() {
-    sliderRow.classList.remove('moving');
+function checkIndex(e) {
+    if(e.target===sliderRow) {
+        sliderRow.classList.remove('moving');
     if(index===-1){
         sliderRow.style.left = finish + 'px';
         index = array.length-1;
@@ -84,101 +97,9 @@ function checkIndex() {
         index = 0;
     }
     allowShift = true;
-}
+}}
 
 }
 
 
 carusel(arrayNumbers10);
-
-
-
-//слайдер передвижение
-/*
-sliderRow.addEventListener('transitionend', ()=> {
-    if(count>=3) {
-        console.log(count); 
-    start = -3351;
-    count=0;
-    sliderRow.style.transition='none';
-    sliderRow.style.left=-3351 + 'px';
-    
-    setTimeout(() => {
-        sliderRow.style.transition = 'all 0.3s';
-      })
-    }
-      });
-
-
-
-//создаем карточку
-
-
-function renderCardLeft(element) {
-    sliderRow.prepend(element);
-}
-
-
-
-//функция зацикливания
-function infiniteSlide(array, dir) {
-    movingClide(dir);
-
-/*
-    if(dir===-1) {
-        renderCardLeft(createCard(array));
-        sliderRow.lastChild.remove();
-    } else if(dir===1) {
-        sliderRow.firstChild.remove();
-    }
-}
-
-/*
-//для зацикливания
-const slides = Array.from(document.querySelectorAll('.slider__element'));
-
-const firstClone = slides[0].cloneNode(true);  //клонируем первый елемент, чтобы он шел после последнего
-const lastClone = slides[slides.length - 1].cloneNode(true); //клонируем последний чтобы он шел перед первым
-
-//slides.append(firstClone);
-//slides.prepend(lastClone);
-
-//примерная функция для зацикливания
-/*
-//составить массив начальный с условием подсчета количества блоков
-функция составляемНачальныйРендер(массив) {
-    если (массив.длина<5) то {
-
-    }
-}
-составление начального массива : 
-for (i=0;i<5;i++) {
-    выбирать из массива [-2,-1, 0,1,2] элементы
-}
-начальный массив[-2,-1, 0,1,2].forEach((card)=>{renderCard(createCard(card))}
-
-функция карусель(напраление: лево 1 или право -1) {
-    если (направлениие:1) то {
-        двигаем блок лево;
-        создаем, добавляем элемент в конец [последний элемент начального массива + 1];
-        удаляем элемент[первый элемент];
-    } иначе {
-        двигаем блок право;
-        создаем, добавляем элемент в начало [первый элемент начального массива - 1];
-        удаляем элемент[последний элемент];
-        }
-}
-
-
-renderCard(createCard(card));
-
-createCard(card){из темплейта
-
-return readyCard
-}
- */
-
-
-
-
-
